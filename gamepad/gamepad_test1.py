53272f55b6fd96d342e3fa4ba7ec45817151332d
Grant Geyer <GrantG012@yahoo.com>
2022-01-30 20:17:35 -0600

    Testing gamepads Jan 14

--- gamepad/gamepad_test1.py
+++ gamepad/gamepad_test1.py
@@ -21,10 +21,31 @@ while True:
     # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
     # JOYBUTTONUP, JOYHATMOTION
     for event in pygame.event.get(): # User did something.
-        print(event)
         if event.type == pygame.QUIT: # If user clicked close.
             done = True # Flag that we are done so we exit this loop.
         elif event.type == pygame.JOYBUTTONDOWN:
             print("Joystick button pressed.")
+            print(event)
+            # print(pygame.key.get_pressed())
+            # a = pygame.key.get_pressed()
+            # al = list(a)
+            # alt = [i for (i, e) in enumerate(al) if e]
+            # print(alt)
+            # print(any(a))
+            # print([joysticks[0].get_button(i) for i in range(joysticks[0].get_numbuttons())])
         elif event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
+            print(event)
+            # a = pygame.key.get_pressed()
+            # al = list(a)
+            # alt = [i for (i, e) in enumerate(al) if e]
+            # print(alt)
+            # print(any(a))
+        elif event.type == pygame.JOYAXISMOTION:
+            print("Joystick moved")
+            print(event)
+        elif event.type == pygame.JOYHATMOTION:
+            print("Joy hat motion")
+            print(event)
+        else:
+            print(event.type)
--- gamepad/gamepad_test2.py
+++ gamepad/gamepad_test2.py
@@ -1,20 +1,40 @@
 """
 
+PS2 - right joy maps to right buttons
+XBOX - 
 """
 
 from inputs import get_gamepad
 
-while True:
-    events = get_gamepad()
-    # print(events)
-    for event in events:
-        # print(event)
-        if event.ev_type == 'Absolute':
-            if event.code == 'ABS_X':
-                print(f'Left joystick x: {event.state}')
-            elif event.code == 'ABS_Y':
-                print(f'Left joystick y: {event.state}')
-            elif event.code == 'ABS_RX':
-                print(f'Right joystick x: {event.state}')
-            elif event.code == 'ABS_RY':
-                print(f'Right joystick y: {event.state}')
+opt = 0
+
+if opt == 0:
+    while True:
+        events = get_gamepad()
+        # print(events)
+        for event in events:
+            # print(event)
+            if(event.ev_type in ["Key", "Absolute"]):
+                print(event.ev_type)
+                try:
+                    print(event.status)
+                except Exception:
+                    pass
+                print(event.code)
+                print(event.state, end='\n\n')
+            # if event.ev_type == 'Absolute':
+            #     if event.code == 'ABS_X':
+            #         print(f'Left joystick x: {event.state}')
+            #     elif event.code == 'ABS_Y':
+            #         print(f'Left joystick y: {event.state}')
+            #     elif event.code == 'ABS_RX':
+            #         print(f'Right joystick x: {event.state}')
+            #     elif event.code == 'ABS_RY':
+            #         print(f'Right joystick y: {event.state}')
+elif opt == 1:
+    from inputs import devices
+    gp = devices.gamepads[0]
+    while True:
+        print(gp.read())
+    # print(gp._GamePad__read_device())
+    # print(gp)
